a9e1e4eb060605059ad38780a1bfb8e1d286ce4f
nadirs <nadirsampaoli@gmail.com>
2022-11-11 19:56:25 +0100

    Add 2015 10

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 pub mod y2015;
 
 fn main() -> Result<(), aoc::Error> {
-    let input = aoc::pull_input(2015, 9)?;
+    let input = aoc::pull_input(2015, 10)?;
 
-    println!("{:?}", y2015::day09::p1(&input));
-    println!("{:?}", y2015::day09::p2(&input));
+    println!("{}", y2015::day10::p1(&input));
+    println!("{}", y2015::day10::p2(&input));
 
     Ok(())
 }
---
+++ src/y2015/day10.rs
@@ -0,0 +1,60 @@
+use std::collections::VecDeque;
+
+pub fn p1(input: &str) -> usize {
+    p1_times(input, 40)
+}
+
+fn p1_times(input: &str, mut times: usize) -> usize {
+    let mut s = input.trim().to_string();
+    while times > 0 {
+        times -= 1;
+        s = look_and_say(&s);
+    }
+
+    s.len()
+}
+
+pub fn p2(input: &str) -> usize {
+    p1_times(input, 50)
+}
+
+fn look_and_say(input: &str) -> String {
+    let groups = input
+        .chars()
+        .into_iter()
+        .fold(VecDeque::new(), |mut acc, d| {
+            if let Some((d0, n)) = acc.back_mut() {
+                if *d0 == d {
+                    *n += 1;
+                } else {
+                    acc.push_back((d, 1));
+                }
+            } else {
+                acc.push_back((d, 1u8))
+            }
+
+            acc
+        });
+
+    groups
+        .into_iter()
+        .flat_map(|(d, n)| [('0' as u8 + n) as char, d])
+        .collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_p1() {
+        assert_eq!(look_and_say("1"), "11");
+        assert_eq!(look_and_say("11"), "21");
+        assert_eq!(look_and_say("21"), "1211");
+        assert_eq!(look_and_say("1211"), "111221");
+        assert_eq!(look_and_say("111221"), "312211");
+
+        assert_eq!(p1_times("1", 5), "312211".len());
+        assert_eq!(look_and_say("1113222113"), "3113322113");
+    }
+}

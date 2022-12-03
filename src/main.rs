c654766ec039a88aa7522239ebbfd8559a2773ef
nadirs <nadirsampaoli@gmail.com>
2022-12-03 13:20:14 +0100

    Add 2022 03

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 pub mod y2022;
 
 fn main() -> Result<(), aoc::Error> {
-    let input = aoc::pull_input(2022, 2)?;
+    let input = aoc::pull_input(2022, 3)?;
 
-    println!("{}", y2022::day02::p1(&input));
-    println!("{}", y2022::day02::p2(&input));
+    println!("{}", y2022::day03::p1(&input));
+    println!("{}", y2022::day03::p2(&input));
 
     Ok(())
 }
---
+++ src/y2022/day03.rs
@@ -0,0 +1,72 @@
+use std::collections::HashSet;
+
+pub fn p1(input: &str) -> usize {
+    input
+        .lines()
+        .map(|line| {
+            let (left, right) = (
+                line.chars()
+                    .take(line.chars().count() / 2)
+                    .collect::<HashSet<char>>(),
+                line.chars()
+                    .skip(line.chars().count() / 2)
+                    .collect::<HashSet<char>>(),
+            );
+
+            let common = *left.intersection(&right).next().unwrap();
+
+            score(common)
+        })
+        .sum()
+}
+
+pub fn p2(input: &str) -> usize {
+    let lines: Vec<_> = input.lines().collect();
+    lines
+        .chunks(3)
+        .map(|chunk| {
+            let mut sets = chunk.iter().map(|s| s.chars().collect::<HashSet<_>>());
+            let first = sets.next().unwrap();
+
+            let common = *sets
+                .fold(first, |acc, other| {
+                    acc.intersection(&other).cloned().collect::<HashSet<char>>()
+                })
+                .iter()
+                .next()
+                .unwrap();
+
+            score(common)
+        })
+        .sum()
+}
+
+fn score(c: char) -> usize {
+    if 'a' <= c && c <= 'z' {
+        c as usize - 'a' as usize + 1
+    } else {
+        c as usize - 'A' as usize + 27
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const INPUT: &str = "vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw";
+
+    #[test]
+    fn test_p1() {
+        assert_eq!(p1(INPUT), 157);
+    }
+
+    #[test]
+    fn test_p2() {
+        assert_eq!(p2(INPUT), 70);
+    }
+}
